a1c2c5b02491fad10435d864d8a27576f0567fe8
RikiXY <riccardino.amato@gmail.com>
2025-04-04 10:01:43 +0200

    addestramento e test

--- Appunti/Scripts/Pix2Pix.py
+++ Appunti/Scripts/Pix2Pix.py
@@ -18,7 +18,7 @@ n_epochs = 100 # Numero di epoche da eseguire
 log_rate = 15 # Ogni quanto loggare (es. ogni 10 batch)
 use_checkpoint = True # Se vuoi riprendere da un checkpoint esistente, metti True       Bisognerebbe creare create_checkpoint e load_checkpoint così si possono distinguere le casiistiche
 checkpoint_rate = 10 # Ogni quanto salvare i checkpoint (es. ogni 10 epoche)
-restore_checkpoint_path = "NOT_Materiale/Locale/checkpoints/checkpoint_Pix2Pix_epoca4_2025-04-03_23-40-20.pth" # Percorso del checkpoint (se esiste), ricordati di cambiare il nome
+restore_checkpoint_path = "Materiale/Locale/checkpoints/checkpoint_Pix2Pix_epoca99_2025-04-04_00-55-08.pth" # Percorso del checkpoint (se esiste), ricordati di cambiare il nome
 validate_rate = 10 # Ogni quanto validare (es. ogni 5 epoche), prendiamo per buono al momento come valore standard =checkpoint_rate
 seed = 42 # Seed per la riproducibilità
 # -----------------------
