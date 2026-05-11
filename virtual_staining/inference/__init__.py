61f856d8c4edca196ec5434d388b75544865d464
Andrea Mura <andreamura.comm@gmail.com>
2026-05-11 19:56:56 +0200

    refactor(inference): remove unused imports from __init__.py

--- virtual_staining/inference/__init__.py
+++ virtual_staining/inference/__init__.py
@@ -1,13 +1,7 @@
 from virtual_staining.inference.config import InferenceConfig
-from virtual_staining.inference.outputs import InferenceOutputWriter
-from virtual_staining.inference.predictor import Predictor
 from virtual_staining.inference.results import InferenceResult
-from virtual_staining.inference.runner import run_inference
 
 __all__ = [
     "InferenceConfig",
-    "run_inference",
-    "Predictor",
-    "InferenceOutputWriter",
     "InferenceResult",
 ]
